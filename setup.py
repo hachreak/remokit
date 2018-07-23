7910175adb5a6e060fcbb5b2420d95438e6789a3
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2018-07-23 10:27:33 +0200

    init repo
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

---
+++ setup.py
@@ -0,0 +1,48 @@
+"""setuptools."""
+
+from setuptools import setup  # , find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+# packages = find_packages('src', exclude=['contrib', 'docs', 'tests'])
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='remokit',
+    version='0.1.0',
+    description='Ant game!',
+    url='https://github.com/hachreak/remokit',
+    author='Leonardo Rossi',
+    author_email='leonardo.rossi@studenti.unipr.it',
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='machine learning',
+    packages={'': 'src'},  # packages,  # Required
+    install_requires=[
+        'numpy',
+        'keras',
+        'tensorflow'
+    ],
+    extras_require={  # Optional
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    }
+)
