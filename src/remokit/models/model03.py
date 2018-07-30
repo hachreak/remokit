1af9ed126bd26d243b8f95153906a937ef57d0c9
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2018-07-30 18:28:54 +0200

    models: new model 03
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

---
+++ src/remokit/models/model03.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of remokit.
+# Copyright 2018 Leonardo Rossi <leonardo.rossi@studenti.unipr.it>.
+#
+# pysenslog is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# pysenslog is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with pysenslog.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Model 03 definition.
+
+See paper: `The Role of Coherence in Facial Expression Recognition`
+"""
+
+from keras import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+
+
+def get_model(input_shape, num_classes):
+    model = Sequential()
+
+    model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1), activation='relu',
+              input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Conv2D(64, (5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(1000, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    return model
