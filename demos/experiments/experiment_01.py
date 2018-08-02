5cb768d0ba1e593c2f744cb8d7c7a09a7cf58165
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2018-08-02 13:02:01 +0200

    demos: fix print seed
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- demos/experiments/experiment_01.py
+++ demos/experiments/experiment_01.py
@@ -43,7 +43,7 @@ def run_experiment(test_index, validation_index, config):
     config['kfold']['test'] = test_index
     config['kfold']['validation'] = validation_index
 
-    print("seed ".format(config['seed']))
+    print("seed {0}".format(config['seed']))
     set_reproducibility(config['seed'])
 
     testing, validating, training = get_tvt_filenames(
