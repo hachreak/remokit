ae09bdc9bb3fa889853d93518b87f7a9852f0d9a
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2018-08-10 16:28:17 +0200

    features: extract faces
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- src/remokit/preprocessing/features.py
+++ src/remokit/preprocessing/features.py
@@ -4,6 +4,18 @@
 from .. import detect
 
 
+def get_face():
+    """Extract face only."""
+    detector = detect.get_detector()
+
+    def f(img):
+        dets = detect.detect(detector, img)
+        for det in dets.values():
+            det = det['detected']
+            return img[det.top():det.bottom(), det.left():det.right()]
+    return f
+
+
 def extract(shape_predictor):
     """Extract features in a face image stream."""
     detector = detect.get_detector()
