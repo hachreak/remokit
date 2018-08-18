5835bb6a6590bb9f69e986a26fbb7ab1a7abd49b
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2018-08-18 11:25:49 +0200

    demos: update exp 03 script
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- demos/experiments/experiment_03_frankenstein.sh
+++ demos/experiments/experiment_03_frankenstein.sh
@@ -4,17 +4,25 @@ validation_index=1
 seed=0
 
 DEMOS="demos/experiments"
-FILES="experiment_01_eyes_model04.json experiment_01_eyesmouth_model04.json
-       experiment_01_face_model04.json experiment_01_mouth_model04.json"
+FILES="experiment_03_frankenstein_eyes.json
+       experiment_03_frankenstein_eyesmouth.json
+       experiment_03_frankenstein_face.json
+       experiment_03_frankenstein_mouth.json"
 
 ONEMODEL="experiment_04_onemodel.py"
+CONFIG="experiment_03_frankenstein.json"
+
+# preprocess images
+for f in $FILES; do
+  echo "preprocess $f"
+  python ${DEMOS}/${ONEMODEL} preprocess ${DEMOS}/$f
+done
 
 # train submodels
 for f in $FILES; do
   echo "Train model [$test_index $validation_index $seed] config file $f"
-  python ${DEMOS}/${ONEMODEL} $test_index $validation_index $seed ${DEMOS}/$f
+  python ${DEMOS}/${ONEMODEL} run ${DEMOS}/$f $test_index $validation_index $seed
 done
 
 # run experiment
-CONFIG=experiment_03_frankenstein.json
 python ${DEMOS}/experiment_01.py run ${DEMOS}/$CONFIG
