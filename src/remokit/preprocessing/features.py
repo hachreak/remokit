3bcf41bd1be6da55b24d49c64b9d45dccbfc5ac8
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2018-08-21 17:22:40 +0200

    features: prepare_batch and save as .npy
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- src/remokit/preprocessing/features.py
+++ src/remokit/preprocessing/features.py
@@ -1,7 +1,10 @@
 
 """Feature extraction."""
 
-from .. import detect
+import os
+import numpy as np
+
+from .. import detect, dataset as ds, adapters, utils
 
 
 def get_face():
@@ -38,3 +41,41 @@ def expand2image(img_x, img_y):
         return img
 
     return f
+
+
+def prepare_batch(config):
+    """Extract faces from the dataset."""
+    gl = utils.load_fun(config['get_label'])
+
+    stream = utils.load_fun(config['get_files'])(**config)
+    stream = ds.stream(ds.add_label(gl), stream)
+    stream = ds.stream(ds.apply_to_x(detect.load_img), stream)
+    stream = ds.stream(
+        ds.apply_to_x(adapters.resize(**config['image_size'])), stream
+    )
+    stream = ds.stream(ds.apply_to_x(adapters.astype('uint8')), stream)
+
+    if config['has_faces']:
+        stream = ds.stream(ds.apply_to_x(get_face()), stream)
+    if config.get('only_features', False):
+        stream = ds.stream(ds.apply_to_x(extract(
+            config['shape_predictor']
+        )), stream)
+    stream = ds.stream(ds.apply_to_x(adapters.astype('uint8')), stream)
+
+    return stream
+
+
+def save(batches, config, indices=None):
+    """Save preprocessed features."""
+    indices = indices or {v: 0 for v in ds._category.keys() + ['index']}
+    print(indices)
+    for X, y in batches:
+        if y == 'neutral':
+            indices['index'] += 1
+        indices[y] += 1
+        filename = '{0:08}_{1}_{2}'.format(indices['index'], y, indices[y])
+        destination = os.path.join(config['destination'], filename)
+        np.save(destination, X)
+        print(destination)
+    return indices
