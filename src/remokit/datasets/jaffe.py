5c3dec925bde34135a7f6357876290b6a77c8fee
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2018-09-03 23:15:02 +0200

    dataset: JAFFE dataset addition
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

---
+++ src/remokit/datasets/jaffe.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of remokit.
+# Copyright 2018 Leonardo Rossi <leonardo.rossi@studenti.unipr.it>.
+#
+# pysenslog is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# pysenslog is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with pysenslog.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Dataset JAFFE
+
+Dataset source: http://www.kasrl.org/jaffe.html
+"""
+
+import os
+from .. import detect, dataset
+
+_label = {
+    'NE': 'neutral',
+    'AN': 'angry',
+    'DI': 'disgust',
+    'FE': 'afraid',
+    'HA': 'happy',
+    'SA': 'sad',
+    'SU': 'surprised',
+}
+
+
+def get_files(directory, *args, **kwargs):
+    """Get image/label files."""
+    return dataset.get_files(directory, types=['.tiff'])
+
+
+def get_data(files_stream):
+    """Get a streaming of label/image to process."""
+    for filename in files_stream:
+        yield detect.load_img(filename), get_label(filename)
+
+
+def get_label(filename):
+    """Convert a 2 chars label to a full name label."""
+    key = os.path.basename(filename)[3:5]
+    return _label[key]
