49ada4bb9b1c2c46b73394b42b30168e01879613
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2018-09-06 23:40:57 +0200

    models: dropout for model 03 help for OOM
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- src/remokit/models/model03.py
+++ src/remokit/models/model03.py
@@ -22,7 +22,7 @@ See paper: `The Role of Coherence in Facial Expression Recognition`
 """
 
 from keras import Sequential
-from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropout
 
 
 def get_model(input_shape, num_classes):
@@ -31,10 +31,14 @@ def get_model(input_shape, num_classes):
     model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1), activation='relu',
               input_shape=input_shape))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
     model.add(Conv2D(64, (5, 5), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
     model.add(Flatten())
     model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.25))
     model.add(Dense(num_classes, activation='softmax'))
 
     return model
